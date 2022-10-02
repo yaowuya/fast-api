53846ce5c1cbac9c9bb568c135c42d738e74b701
yaowuya <zrfyaowuya@126.com>
2022-10-02 14:56:32 +0800

    蓝图简单实现

---
+++ ginger/app/api/v1/user.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+user = Blueprint('user', __name__)
+
+
+@user.route('/v1/user/get')
+def get_user():
+    return 'i am ruifeng'
--- ginger/app/app.py
+++ ginger/app/app.py
@@ -1,8 +1,18 @@
 from flask import Flask
 
 
+def register_blueprints(app: Flask):
+    from app.api.v1.user import user
+    from app.api.v1.book import book
+    app.register_blueprint(user)
+    app.register_blueprint(book)
+
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.secure')
     app.config.from_object('app.config.setting')
+
+    register_blueprints(app)
+
     return app
